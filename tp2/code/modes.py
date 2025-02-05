123182e69e75f620a027d2a3aaefe28c592aa815
sl-Zhou <113488440+sl-Zhou@users.noreply.github.com>
2025-02-05 13:04:07 -0500

    Delete tp2/code/modes.py

--- tp2/code/modes.py
+++
@@ -1,7 +0,0 @@
-'''
-    This file contains some constants to help manage the app's two
-    display modes, Percent and Count.
-'''
-
-MODES = dict(count='Count', percent='Percent')
-MODE_TO_COLUMN = {MODES['count']: 'LineCount', MODES['percent']: 'LinePercent'}
